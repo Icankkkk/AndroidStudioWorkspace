e399bcbd6b22a05c338d59dc43c3561584ecd468
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2022-03-26 08:21:36 +0700

    Menambahkan project Logika

---
+++ Logika/app/src/main/java/com/icang/logika/MainActivity.java
@@ -0,0 +1,41 @@
+package com.icang.logika;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvNilaiHasil, tvGrade;
+    EditText edtInputNilai;
+    Button btLogika;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvNilaiHasil = findViewById(R.id.tv_nilai_result);
+        tvGrade = findViewById(R.id.tv_grade_result);
+        edtInputNilai = findViewById(R.id.edt_nilai);
+        btLogika = findViewById(R.id.btn_logic);
+        btLogika.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_logic) {
+            double nilai = Double.parseDouble(edtInputNilai.getText().toString());
+            if ( nilai >= 80) {
+                tvNilaiHasil.setText(Double.toString(nilai));
+                tvGrade.setText("A");
+            } else {
+                tvNilaiHasil.setText(Double.toString(nilai));
+                tvGrade.setText("B");
+            }
+        }
+    }
+}
